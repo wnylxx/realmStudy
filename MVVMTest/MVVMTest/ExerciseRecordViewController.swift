1de829e8d7610131d075b564825383beb0d75592
WonyeolHEO <hyy96hyy@naver.com>
2024-09-02 21:59:15 +0900

    ReportView setup UI

---
+++ MVVMTest/MVVMTest/ExerciseRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExerciseRecordViewController.swift
+//  MVVMTest
+//
+//  Created by wonyoul heo on 9/2/24.
+//
+
+import UIKit
+
+class ExerciseRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MVVMTest/MVVMTest/ReportsViewController.swift
@@ -0,0 +1,180 @@
+//
+//  ViewController.swift
+//  MVVMTest
+//
+//  Created by wonyoul heo on 9/2/24.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+    
+    private var currentVC: UIViewController?
+    private let exerciseRecordVC = ExerciseRecordViewController()
+    private let weightRecordVC = WeightRecordViewController()
+
+    private lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+        
+    }()
+    
+    private lazy var moveToPreviousMonthButton: UIButton = {
+        let button = createMonthButton(action: UIAction {[weak self] _ in
+            self?.didTapPreviousMonth()
+        }, imageName: "chevron.left")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var titleMonthLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2024년 7월 리포트"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    private lazy var moveToNextMonthButton: UIButton = {
+        let button = createMonthButton(action: UIAction {[weak self] _ in
+            self?.didTapNextMonth()
+        }, imageName: "chevron.right")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var segmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl()
+        control.insertSegment(withTitle: "운동 기록", at: 0, animated: true)
+        control.insertSegment(withTitle: "인바디 기록", at: 1, animated: true)
+        control.selectedSegmentIndex = 0
+        control.addTarget(self, action: #selector(didChangeValue(_:)), for: .valueChanged)
+        
+        
+        control.selectedSegmentTintColor = .green
+        control.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        
+        control.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "Pretendard-SemiBold", size: 13) ?? UIFont.systemFont(ofSize: 13)
+        ], for: .normal)
+        
+        control.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "Pretendard-SemiBold", size: 13) ?? UIFont.systemFont(ofSize: 13)
+        ], for: .selected)
+        
+        control.translatesAutoresizingMaskIntoConstraints = false
+        return control
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        
+    }
+
+        
+    
+    
+    
+    private func createMonthButton(action: UIAction, imageName: String) -> UIButton {
+        let button = UIButton(type: .custom)
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 12, weight: .black)
+        var config = UIButton.Configuration.plain()
+        config.image = UIImage(systemName: imageName, withConfiguration: symbolConfig)
+        config.baseForegroundColor = .white
+        button.configuration = config
+        
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }
+    
+    
+    
+    private func didTapPreviousMonth() {
+        
+    }
+    
+    private func didTapNextMonth() {
+        
+        
+    }
+    
+    @objc private func didChangeValue(_ sender: UISegmentedControl) {
+        let selectedIndex = sender.selectedSegmentIndex
+        
+        currentVC?.willMove(toParent: nil)
+        currentVC?.view.removeFromSuperview()
+        currentVC?.removeFromParent()
+        
+        let newVC: UIViewController
+        if selectedIndex == 0 {
+            newVC = exerciseRecordVC
+        } else {
+            newVC = weightRecordVC
+        }
+        
+        addChild(newVC)
+        newVC.didMove(toParent: self)
+        currentVC = newVC
+        
+    }
+    
+    
+}
+
+
+extension ReportsViewController {
+    
+    private func setupUI() {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.view.backgroundColor = .gray
+        
+        titleStackView.addArrangedSubview(moveToPreviousMonthButton)
+        titleStackView.addArrangedSubview(titleMonthLabel)
+        titleStackView.addArrangedSubview(moveToNextMonthButton)
+    
+        self.view.addSubview(titleStackView)
+        self.view.addSubview(segmentedControl)
+        
+        
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            titleStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
+            titleStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
+            
+            segmentedControl.topAnchor.constraint(equalTo: titleStackView.bottomAnchor, constant: 13),
+            segmentedControl.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
+            segmentedControl.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
+            
+        ])
+        
+        let initialViewController = exerciseRecordVC
+        addChild(initialViewController)
+        initialViewController.didMove(toParent: self)
+        currentVC = initialViewController
+    }
+    
+    override func addChild(_ viewController: UIViewController) {
+        view.addSubview(viewController.view)
+        
+        // 레이아웃 설정
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            viewController.view.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20),
+            viewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            viewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
+            viewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
